ac884ab27b4e9b01ae03ae50e95fd192aa5f3702
W6Matt <75140430+W6Matt@users.noreply.github.com>
2023-09-15 16:24:33 -0600

    new

--- cmake-build-release/CS330M/cmake_install.cmake
+++ cmake-build-release/CS330M/cmake_install.cmake
@@ -37,3 +37,8 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2022.2.1/bin/mingw/bin/objdump.exe")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/matt/source/repos/CS330M/cmake-build-release/CS330M/external/cmakelibs/glfw/cmake_install.cmake")
+endif()
+
