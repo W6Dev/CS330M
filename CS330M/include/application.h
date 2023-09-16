9facbdad0b7e39df4e67846304d275d5b7767def
W6Matt <75140430+W6Matt@users.noreply.github.com>
2023-09-15 20:16:45 -0600

    new

--- CS330M/include/application.h
+++ CS330M/include/application.h
@@ -3,7 +3,7 @@
 #include <string>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
+#include <mesh.h>
 
 
 class Application
@@ -14,11 +14,23 @@ public:
 
 private:
 	bool openWindow();
+    bool draw();
+    bool update();
+
+    void setupScene();
 
 private:
 	std::string _applicationName{};
 	int _width{};
 	int _height{};
 	GLFWwindow* _window{nullptr};
+    GLuint _vertexBufferObject{};
+    GLuint _shaderProgram{};
+    GLuint _vertexArrayObject{};
+    GLuint _elementBufferObject{};
+
+    std::vector<Mesh> _meshes;
+
+
 	bool _running{false};
 };
--- CS330M/src/application.cpp
+++ CS330M/src/application.cpp
@@ -1,65 +1,124 @@
 #include <application.h>
 #include <iostream>
+#include <types.h>
+#include <vector>
+
+Application::Application(std::string WindowTitle, int width, int height) : _applicationName(WindowTitle), _width(width),
+                                                                          _height(height) {}
+
+void Application::Run() {
+    // open a window
+    if (!openWindow()) {
+        return;
+    };
+
+    _running = true;
+
+    // setup the scene
+    setupScene();
 
 
-Application::Application(std::string WindowTitle, int width, int height) : _applicationName(WindowTitle), _width(width), _height(height){}
+    // run application
+    while (_running) {
+        if (glfwWindowShouldClose(_window)) {
+            _running = false;
+            break;
+        }
 
+        update();
+        draw();
+    }
 
-void Application::Run()
-{
-	// open a window
-	if (!openWindow())
-	{
-		return;
-	};
-
-	_running = true;
-
-	// run application
-	while (_running)
-	{
-		if (glfwWindowShouldClose(_window))
-		{
-			_running = false;
-			break;
-		}
-		glfwSwapBuffers(_window);
-		glfwPollEvents();
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-	}
-	glfwTerminate();
+    glfwTerminate();
 }
 
-bool Application::openWindow()
-{
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+bool Application::openWindow() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	_window = glfwCreateWindow(800, 600, "CS330 Matt Sandoval", nullptr, nullptr);
+    _window = glfwCreateWindow(800, 600, "CS330 Matt Sandoval", nullptr, nullptr);
 
-	if (!_window)
-	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		
-	}
-	glfwMakeContextCurrent(_window);
-	glfwSetFramebufferSizeCallback(_window, [](GLFWwindow* window, int width, int height)
-	{
-		// Resize the viewport when the window is resized
-		glViewport(0, 0, width, height);
-	});
+    if (!_window) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+
+    }
+    glfwMakeContextCurrent(_window);
+    glfwSetFramebufferSizeCallback(_window, [](GLFWwindow *window, int width, int height) {
+        // Resize the viewport when the window is resized
 
 
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cerr << "Failed to initialize GLAD" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
+        glViewport(0, 0, width, height);
+    });
 
-	
+
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        glfwTerminate();
+        return false;
+    }
+    return true;
+
 }
+
+void Application::setupScene() {
+    std::vector<Vertex> vertices = {
+            {
+
+                    .Position = {-0.5f, 0.5f, 0.0f},
+                    .Color = {1.0f, 0.0f, 0.0f}
+
+            },
+            {
+                    .Position = {-0.5f, -0.5f, 0.0f},
+                    .Color = {0.0f, 1.0f, 0.0f}
+            },
+            {
+                    .Position = {0.5f, -0.5f, 0.0f},
+                    .Color = {0.0f, 0.0f, 1.0f}
+            },
+            {
+                    .Position = {0.5f, 0.5f, 0.0f},
+                    .Color = {1.0f, 0.0f, 0.0f}
+            }
+
+
+    };
+
+    std::vector<uint32_t> elements = {
+            0, 1, 2,
+            0, 2, 3
+    };
+
+    _meshes.emplace_back(std::move(vertices), std::move(elements));
+
+    vertices.clear();
+    elements.clear();
+}
+
+
+bool Application::update() {
+
+    return false;
+}
+
+bool Application::draw() {
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    for(auto mesh : _meshes)
+    {
+        mesh.Draw();
+    }
+
+    glfwSwapBuffers(_window);
+    glfwPollEvents();
+
+    return false;
+}
+
+
+
+
